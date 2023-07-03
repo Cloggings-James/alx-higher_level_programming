bb2f96b650c87913977ec6a11b0e42e96f1df342
Cloggings-James <James.wanjiru.sheba@gmail.com>
2023-07-03 05:46:02 -0700

    muchemni

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Rectangle Module
+Defines the Rectangle class
+"""
+
+
 class Rectangle:
+    """
+    Rectangle class
+    """
     pass
+
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
 
