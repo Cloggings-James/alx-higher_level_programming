c6716112adb191d1d16032f99da2120564bea453
cloggings-James <james.wanjiru.sheba@gmail.com>
2023-09-22 13:57:45 -0700

    val

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
-"""Script to filter states by name starting with 'N' using SQLAlchemy"""
+"""
+Lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa
+"""
 
-import sys
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb as mysql
 
-# Define the database connection URL
-DATABASE_URL = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-    sys.argv[1], sys.argv[2], sys.argv[3])
-
-# Create a database engine
-engine = create_engine(DATABASE_URL)
-
-# Create a session to interact with the database
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Define the State model
-Base = declarative_base()
-
-
-class State(Base):
-    """State model"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(256), nullable=False)
-
-# Filter and print states starting with 'N'
-if __name__ == "__main__":
     try:
-        # Query for states starting with 'N'
-        states = session.query(State).filter(State.name.like('N%')).order_by(State.id).all()
+        db = mysql.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    except Exception:
+        print('Failed to connect to the database')
+        exit(0)
 
-        # Display the results
-        for state in states:
-            print("({}, '{}')".format(state.id, state.name))
+    cursor = db.cursor()
 
-        # Close the session
-        session.close()
+    cursor.execute("SELECT * FROM states \
+                    WHERE name LIKE BINARY 'N%' ORDER BY id ASC;")
 
-    except Exception as e:
-        print("Error:", e)
-        sys.exit(1)
+    result_query = cursor.fetchall()
 
+    for row in result_query:
+        print(row)
+
+    cursor.close()
+    db.close()
